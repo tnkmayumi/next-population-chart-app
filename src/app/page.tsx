04597f02f7b2d0f9e1abe023486220a1acff956d
tnkmayumi <miiy.16f@gmail.com>
2024-11-30 16:12:57 +0900

    init Next.js project

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+// import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <main>初期設定完了</main>
+      <footer>
+        <nav></nav>
+      </footer>
+    </div>
+  );
+}
